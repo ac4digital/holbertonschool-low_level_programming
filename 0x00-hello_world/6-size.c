e1f9eb24dc2771993ac39570c1f6830bf14059ec
Andrés Cataño <3866@holbertonschool.com>
2021-09-30 22:10:27 -0500

    C Program prints the size of various types

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -3,21 +3,21 @@
 /**
  * main - Prints size of type
  *
- * Return 0 (Success)
+ * Return: 0 (Success)
  */
 
-int main(void)
+int main(void) /* Returns*/
 {
 	/* Variable Initialisation */
 	char v_character;
 	int v_integer;
 	long int v_long;
-	long long v_long_long;
+	long long int v_long_long;
 	float v_float;
 	/* Prints */
 	printf("Size of a char: %lu byte(s)\n", sizeof(v_character));
 	printf("Size of an int: %lu byte(s)\n", sizeof(v_integer));
-	printf("Size of a long int: %d byte(s)\n", sizeof(v_long));
+	printf("Size of a long int: %lu byte(s)\n", sizeof(v_long));
 	printf("Size of a long long int: %lu byte(s)\n", sizeof(v_long_long));
 	printf("Size of a float: %lu byte(s)\n", sizeof(v_float));
 	return (0);
