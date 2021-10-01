6b33cc2b3e98bfb1babd5500f25739854d89f233
Andrés Cataño <3866@holbertonschool.com>
2021-09-30 20:02:29 -0500

    C Program prints a quote

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,16 @@
+/*
+ * 4.Hello, puts
+ *
+ */
+
+#include<stdio.h>
+
+int main(void)
+{
+	/* initialize var */
+	char MyQuote[] = "Programming is like building a multilingual puzzle\n";
+	/* print the quote */
+	puts(MyQuote);
+
+	return (0);
+}
