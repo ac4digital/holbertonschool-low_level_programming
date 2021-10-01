facf2816cdafb21be5f83425869be2cc287a91fc
Andrés Cataño <3866@holbertonschool.com>
2021-09-30 22:06:07 -0500

    C Program prints a quote without using printf of puts

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <unistd.h>
+
+
+/**
+ * main - Prints quote without using printf or puts
+ *
+ * Return: (1)
+ *
+ */
+
+int main(void)
+{
+	write(1, "and that piece of art is usefull\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
+}
