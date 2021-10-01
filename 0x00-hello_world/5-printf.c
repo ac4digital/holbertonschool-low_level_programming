b0b6d30e0086a0f1805e06133333de3f151a05b2
Andrés Cataño <3866@holbertonschool.com>
2021-09-30 20:42:34 -0500

    C program prints a quote

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -10,9 +10,7 @@ int main(void)
 {
 	/* Variable Initialisation */
 	char Message[] = "with proper grammar, but the outcome is a piece of art,\n";
-	
 	/* Print Message */
-	printf("%s", Message);
-	
+	printf("%s", Message);	
 	return (0);
 }
