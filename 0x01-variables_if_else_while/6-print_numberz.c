6d4c1751ec7089c768fc6b4e7183955d04c319f0
Andrés Cataño <3866@holbertonschool.com>
2021-10-01 20:37:17 -0500

    Program prints all single digit numbers of base 10

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Prints all single digit numbers of base 10
+ *
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	int number_base_10 = 0;
+
+	do {
+		putchar(number_base_10);
+		number_base_10++;
+	} while (number_base_10 < 10);
+
+	putchar('\n');
+
+	return (0);
+}
