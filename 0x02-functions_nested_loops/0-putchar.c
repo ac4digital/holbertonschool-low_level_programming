d2e1cd56d6006b6599668100320eac635e00e621
Andrés Cataño <3866@holbertonschool.com>
2021-10-05 10:01:01 -0500

    Program prints _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the word _putchar
+ *
+ * Return: 0 Success
+ */
+
+int main(void)
+{
+	char word[8] = "_putchar";
+	int i;
+
+	for (i = 0; i <= 8; i++)
+	{
+		putchar(word[i]);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
+
+
