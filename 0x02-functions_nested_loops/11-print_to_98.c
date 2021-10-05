e053220288d7c5c283324a982209eb9248bd8778
Andrés Cataño <3866@holbertonschool.com>
2021-10-05 09:32:25 -0500

    Function prints numbers from n to 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -11,20 +11,15 @@
 
 void print_to_98(int n)
 {
-	if (n == 98)
+	if (n < 98)
 	{
-		printf("%d", n);
-	}
-	else if (n < 98)
-	{
-		for (n = n; n <= 98; n++)
+		for (n = n; n < 98; n++)
 			printf("%d, ", n);
 	}
 	else
 	{
-		for (n = n; n >= 98; n--)
+		for (n = n; n > 98; n--)
 			printf("%d, ", n);
 	}
-	printf("\n");
+	printf("98\n");
 }
-
