d687f7b1c4eab9386e514c572f549515ccf5c9df
Andrés Cataño <3866@holbertonschool.com>
2021-10-07 10:30:30 -0500

    Debugging - Commented infinite loop

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -1,18 +1,19 @@
 #include <stdio.h>
 
 /**
-* main - causes an infinite loop
+* main - Comment causes an infinite loop
+*
 * Return: 0
 */
 
 int main(void)
 {
 
-	int i;
+	/*int i;*/
 
 	printf("Infinite loop incoming :(\n");
 
-	i = 0;
+	/*i = 0;*/
 
 	/* while (i < 10) */
 	/* Infinite loop, there is not increment for i */
