dd30a0a663d217d68166d5031507cc261fb4abc7
Andrés Cataño <3866@holbertonschool.com>
2021-10-07 10:27:11 -0500

    Debugging - Commented infinite loop

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -7,19 +7,19 @@
 
 int main(void)
 {
+
 	int i;
-	
+
 	printf("Infinite loop incoming :(\n");
-	
+
 	i = 0;
-	
+
 	/* while (i < 10) */
 	/* Infinite loop, there is not increment for i */
-	{
-		putchar(i);
-	}
-	
+	/*{*/
+	/*putchar(i);*/
+	/*}*/
 	printf("Infinite loop avoided! \\o/\n");
-	
+
 	return (0);
 }
