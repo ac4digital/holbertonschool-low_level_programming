4ccc2b7d9343fafc506fa4e5b9a0f5f8aadef915
Andrés Cataño <3866@holbertonschool.com>
2021-10-19 16:27:36 -0500

    Even more pointers, arrays and strings

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -10,13 +10,12 @@ char *_strchr(char *s, char c)
 {
 	int index;
 
-	for (index = 0; s[index] != '\0' ; index++)
+	for (index = 0; *(s + index); index++)
 	{
-		if (s[index] == c)
-		{
-			return (&s[index]);
-		}
+		if (*(s + index) == c)
+			return (s + index);
 	}
-	/*return (&s[index]);*/
-	return (NULL);
+	if (*(s + index) == c)
+		return (s + index);
+	return (0);
 }
