59cc32ab4181a10757c3e1ea45b5fe42612fc380
Andrés Cataño <3866@holbertonschool.com>
2021-10-22 23:07:58 -0500

    C Argc, Argv

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,57 +1,46 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
- * coins - count how many coins
- * @value: value to change
- * Return: number of coins
- */
-int coins(int value)
-{
-	int var_coins = 0;
-
-	if (value >= 25)
-		var_coins = coins(value - 25) + 1;
-	if (value >= 10 && value < 25)
-		var_coins = coins(value - 10) + 1;
-	if (value >= 5 && value < 10)
-		var_coins = coins(value - 5) + 1;
-	if (value >= 2 && value < 5)
-		var_coins = coins(value - 2) + 1;
-	if (value >= 1 && value < 2)
-		var_coins = coins(value - 1) + 1;
-
-	return (var_coins);
-}
-
-/**
- * main - function prints the minimun of coins to make change
- * for an amount of money
- * @argc: length argument
- * @argv: array argument
- * Return: 0 Success otherwise 1
- */
+ * main - prints the minimum number of coins to make change for a given amount
+ * @argc: arguement count
+ * @argv: array of pointers to arguement strings
+ * Return: number of coins or 1
+ **/
 int main(int argc, char *argv[])
 {
-	int value  = 0;
+	int amount, coins;
 
 	if (argc != 2)
 	{
 		printf("Error\n");
 		return (1);
 	}
-
-	value = atoi(argv[1]);
-
-	if (value < 0)
+	amount = atoi(argv[1]);
+	coins = 0;
+	if (amount > 25)
 	{
-		printf("0\n");
-		return (0);
+		while (amount >= 25)
+			amount -= 25, coins++;
 	}
-
-	if (value > 0)
+	if (amount > 10 && amount < 25)
 	{
-		printf("%d\n", coins(value));
+		while (amount >= 10)
+			amount -= 10, coins++;
 	}
-
+	if (amount > 5 && amount < 10)
+	{
+		while (amount >= 5)
+			amount -= 5, coins++;
+	}
+	if (amount > 2 && amount < 5)
+	{
+		while (amount >= 2)
+			amount -= 2, coins++;
+	}
+	if (amount == 1 || amount == 2 || amount == 5 || amount == 10 || amount == 25)
+	{
+		coins++;
+	}
+	printf("%d\n", coins);
 	return (0);
 }
