712e41baaf830ba828c86ec346cdbd861bbccce3
Andrés Cataño <3866@holbertonschool.com>
2021-11-04 17:31:08 -0500

    C - Variadic functions - Tasks

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -19,7 +19,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 		{
 			printf("%d", va_arg(list_args, int));
 			if (i < n - 1 && separator)
-				printf("%c ", *separator);
+				printf("%s", separator);
 		}
 		va_end(list_args);
 
