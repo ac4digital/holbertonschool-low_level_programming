962f2072dc1f532beaf64fef7198a2efc4f81dc9
Andrés Cataño <3866@holbertonschool.com>
2021-11-05 13:41:55 -0500

    C - Function pointers - Tasks

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,9 +1,7 @@
 #include "function_pointers.h"
-#include <stdlib.h>
-#include <stdio.h>
 /**
  * print_name - function that prints a name
- * @name: string
+ * @name: string to print
  * @f: pointer to the function
  * Return: nothing
  */
@@ -11,7 +9,5 @@
 void print_name(char *name, void (*f)(char *))
 {
 	if (name != NULL && f != NULL)
-	{
 		f(name);
-	}
 }
