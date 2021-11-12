f0ac66e58ba9a3e1f79eddac4744e77bd14dfd2b
Andrés Cataño <3866@holbertonschool.com>
2021-11-12 00:01:12 -0500

    Free a singly linked list

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -12,16 +12,12 @@
  */
 void free_list(list_t *head)
 {
-	list_t *next;
-
-	while (head != NULL)
+	if (head != NULL)
 	{
-		if (head->str != NULL)
-			free(head->str);
+		if (head->next != NULL)
+			free_list(head->next);
 
-		next = head->next;
+		free(head->str);
 		free(head);
-		head = next;
 	}
-
 }
