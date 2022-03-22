da2c89b99c6d96b3e57f3c60d011c1d686cae504
Andrés Cataño <3866@holbertonschool.com>
2022-03-21 19:27:05 -0500

    First Commit

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -5,10 +5,10 @@ def island_perimeter(grid):
     """ Returns the perimeter of the island """
     perimeter = 0
     length = len(grid) - 1
-    width = len(grid) - 1
+    width = len(grid[0]) - 1
 
     for i, r in enumerate(grid):
-        for j, n in enumerate(grid):
+        for j, n in enumerate(r):
             if n == 1:
                 if i == 0 or grid[i - 1][j] != 1:
                     perimeter += 1
